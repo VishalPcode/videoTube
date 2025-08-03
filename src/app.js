2a24f397a5ae2693e6edc8ac374155363d19cba0
VishalPcode <vishalgupta732160@gmail.com>
2025-08-03 22:16:37 +0530

    Refactor CORS middleware to use allowed origins array for improved security

--- src/app.js
+++ src/app.js
@@ -5,10 +5,27 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 // CORS middleware
+// app.use(cors({
+//   origin: "https://desivibe.netlify.app", // your frontend URL
+//   credentials: true
+// }));
+
+const allowedOrigins = [
+  "https://desivibe.netlify.app",
+  "http://localhost:5173"
+];
+
 app.use(cors({
-  origin: "https://desivibe.netlify.app", // your frontend URL
-  credentials: true
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
 }));
+
 
 // Core middlewares
 app.use(express.json({ limit: "16kb" }));
